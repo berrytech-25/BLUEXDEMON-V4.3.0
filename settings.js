02d8dccf609b904351a9be6d74b16cd0f90674e7
berrytech-25 <jrockstock51@gmail.com>
2025-01-17 12:55:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ global.antidelete = false
 global.autotyping = false
 global.autoBio = true
 global.antiSpam = true
-global.sign = "𓃵"
+global.sign = "👁️‍🗨️"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
 global.bankname = "Victor Akpan"
